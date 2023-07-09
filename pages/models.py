c103bfc0714dba9716b282934f763e9643b4180d
Yahya Ahmed <Yahya.Ahmed@DESKTOP>
2023-07-09 23:35:40 +0300

    added model and views pages

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Login(models.Model):
+    username = models.CharField( max_length=50)
+    mail=models.EmailField(max_length=30)
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Login
+from django.contrib import messages # new
+
 # Create your views here.
+# 
 def index(request):
-    return render(request,'pages/index.html')
+    if request.method == 'POST': # new
+        username = request.POST.get('username')
+        mail = request.POST.get('mail')
+        if username and mail: # new
+            try: # new
+                data = Login(username=username, mail=mail)
+                data.save()
+                messages.success(request, f'Your data has been saved!') # new
+                return redirect('some_other_page') # new
+            except Exception as e: # new
+                messages.error(request, f'Something went wrong: {e}') # new
+        else: # new
+            messages.error(request, f'Please enter a valid username and mail.') # new
+    return render(request, 'pages/index.html')
